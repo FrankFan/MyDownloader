d0c9bbf2b61cfb9159f36ba37484f7273c119f3f
FrankFan <fanyong@gmail.com>
2014-01-14 23:14:39 +0800

    add new files

---
+++ SimpleDownloader/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace SimpleDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenFileDir = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.lblLink = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(39, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "下载进度：";
+            // 
+            // btnOpenFileDir
+            // 
+            this.btnOpenFileDir.Location = new System.Drawing.Point(320, 109);
+            this.btnOpenFileDir.Name = "btnOpenFileDir";
+            this.btnOpenFileDir.Size = new System.Drawing.Size(94, 23);
+            this.btnOpenFileDir.TabIndex = 15;
+            this.btnOpenFileDir.Text = "打开文件位置";
+            this.btnOpenFileDir.UseVisualStyleBackColor = true;
+            this.btnOpenFileDir.Click += new System.EventHandler(this.btnOpenFileDir_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(180, 110);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 14;
+            this.btnOpenFile.Text = "打开文件";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(119, 157);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(315, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(43, 111);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 12;
+            this.btnStart.Text = "开始下载";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(41, 53);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(393, 21);
+            this.txtLink.TabIndex = 9;
+            // 
+            // lblLink
+            // 
+            this.lblLink.AutoSize = true;
+            this.lblLink.ForeColor = System.Drawing.Color.Red;
+            this.lblLink.Location = new System.Drawing.Point(39, 24);
+            this.lblLink.Name = "lblLink";
+            this.lblLink.Size = new System.Drawing.Size(101, 12);
+            this.lblLink.TabIndex = 8;
+            this.lblLink.Text = "请输入下载链接：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 295);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpenFileDir);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.lblLink);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpenFileDir;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Label lblLink;
+    }
+}
+
---
+++ SimpleDownloader/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+namespace SimpleDownloader
+{
+    public partial class Form1 : Form
+    {
+        //定义时间全局变量
+        //定时器
+        Timer timer = new Timer();
+        //时间数
+        long timeCount = 0;
+
+        string url = string.Empty;
+        string fileName = string.Empty;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //加载窗体时，不激发计时器事件
+            timer.Enabled = false;
+            timer.Interval = 1000;
+        }
+
+        /// <summary>
+        /// 开始下载
+        /// </summary>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //下载
+            //计时器开始
+            timer.Enabled = true;
+            timer.Tick += new EventHandler(timer_Tick);
+
+            
+
+
+            //使用using自动释放资源
+            using (WebClient wc = new WebClient())
+            {
+                //使用WebClient下载数据
+                //判断下载地址是否合法
+                if (string.IsNullOrEmpty(txtLink.Text.Trim()))
+                {
+                    MessageBox.Show("请输入下载地址", "提示");
+                }
+                else
+                { //开始下载
+                    try
+                    {
+                        url = txtLink.Text.Trim();
+                        fileName = url.Substring(url.LastIndexOf('/') + 1);
+
+                        //Uri：统一资源标示符
+                        Uri address = new Uri(url);
+                        //1.调用DownFile下载文件到本地
+                        //注意：使用此方法会使窗体界面卡住，不推荐使用
+                        //wc.DownloadFile(address, txtFilename.Text.Trim());                        
+
+                        //2.推荐使用DownloadFileAsync异步下载文件
+                        wc.DownloadFileAsync(address, fileName);
+
+                        //如果不出错的话，文件已经下载到本地了。
+
+                        //进度条显示
+                        wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(wc_DownloadProgressChanged);
+
+                        //下载完毕提示
+                        wc.DownloadFileCompleted += new AsyncCompletedEventHandler(wc_DownloadFileCompleted);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //抛出异常
+                        MessageBox.Show(ex.Message, "出错啦~~");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 打开文件
+        /// </summary>
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                MessageBox.Show("您还未下载文件哦~");
+            }
+            else
+            {
+                //打开文件的方法
+                System.Diagnostics.Process.Start(fileName); 
+            }
+        }
+
+        /// <summary>
+        /// 打开文件目录
+        /// </summary>
+        private void btnOpenFileDir_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(Application.StartupPath);
+        }
+
+        /// <summary>
+        /// 计时器的执行委托
+        /// </summary>
+        void timer_Tick(object sender, EventArgs e)
+        {
+            timeCount++;
+        }
+
+        /// <summary>
+        /// 定义进度条响应事件
+        /// </summary>
+        private void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        /// <summary>
+        /// 提示下载完成，并显示下载文件所用时
+        /// </summary>
+        private void wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("文件下载完毕！共用时：" + timeCount + " 秒", "恭喜~~下载完成~~",
+                            MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+    }
+}
